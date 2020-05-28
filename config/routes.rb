7a1d61cf655eee7969e556f096312bc12502ee73
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2020-05-28 23:04:56 +0100

    added CRUD functionality for bank account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
-  resources :bank_accounts, only: [:index, :show]
+  resources :bank_accounts
 
   namespace :api do
     namespace :v1 do
